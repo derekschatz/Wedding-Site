204a48fcd49261f287da1d9d5a68c586bdcd01bf
derekschatz <derekschatz1@gmail.com>
2019-06-03 19:56:51 -0400

    Resetting

--- js/scripts.js
+++ js/scripts.js
@@ -147,7 +147,7 @@ $(document).ready(function () {
 
     for (var i = 0; i < share_bar.length; i++) {
         var html = '<iframe allowtransparency="true" frameborder="0" scrolling="no"' +
-            'src="https://platform.twitter.com/widgets/tweet_button.html?url=' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;hashtags=heimbecomingaschatz&amp;count=horizontal"' +
+            'src="https://platform.twitter.com/widgets/tweet_button.html?url=' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;via=ramswarooppatra&amp;hashtags=ramandantara&amp;count=horizontal"' +
             'style="width:105px; height:21px;">' +
             '</iframe>' +
 
@@ -184,20 +184,20 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Derek and Alex's Wedding",
+            title: "Ram and Antara's Wedding",
 
             // Event start date
-            start: new Date('Jun 5, 2020 8:00'),
+            start: new Date('Nov 27, 2017 10:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Jun 5, 2020 12:00PM'),
+            end: new Date('Nov 29, 2017 00:00'),
 
             // Event Address
-            address: 'Philadelphia Cricket Club',
+            address: 'ITC Fortune Park Hotel, Kolkata',
 
             // Event Description
             description: "We can't wait to see you on our big day. For any queries or issues contact Mr. Amit Roy at +91 9435021804 or +91 7086018971."
@@ -214,10 +214,11 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== '2ae0a8a501f1ae89cf0fbee0dd684bd5') {
+        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycby1tEXMc-s7Ygu5x3sgGzptYBKM3v8VvszaCXWMV6-zJQWZWH4/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
                 .done(function (data) {
                     console.log(data);
                     $('#alert-wrapper').html('');
@@ -232,15 +233,14 @@ $(document).ready(function () {
 
 });
 
-
 /********************** Extras **********************/
 
 // Google map
 function initMap() {
-    var pcc = {lat: 40.065180, lng: -75.209274};
+    var itc_kol = {lat: 22.5932759, lng: 88.27027720000001};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
-        center: ppc,
+        center: itc_kol,
         scrollwheel: false
     });
 
@@ -250,6 +250,19 @@ function initMap() {
     });
 }
 
+function initBBSRMap() {
+    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var map = new google.maps.Map(document.getElementById('map-canvas'), {
+        zoom: 15,
+        center: la_fiesta,
+        scrollwheel: false
+    });
+
+    var marker = new google.maps.Marker({
+        position: la_fiesta,
+        map: map
+    });
+}
 
 // alert_markup
 function alert_markup(alert_type, msg) {
